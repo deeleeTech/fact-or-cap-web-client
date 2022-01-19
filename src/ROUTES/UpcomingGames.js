38585a4a49003becf027bf394a49805ab6aff3af
Dillon Lee <deeleeTech@outlook.com>
2022-01-18 22:50:02 -0600

    added date naviagtor and filter of games

--- src/ROUTES/UpcomingGames.js
+++ src/ROUTES/UpcomingGames.js
@@ -27,24 +27,65 @@ export default function UpcomingGames(props) {
     const navigate = useNavigate();
     const allGames = useSelector(state => state.allNBA);
     const [ todaysGames, setTodaysGames ] = useState(null);
-    const [ filteredGames, setFilteredGames ] = useState(null);
+
+
+    const [ toggledDate, setToggledDate ] = useState(new Date())
 
     useEffect(()=>{
         //filter out todays games
         
     },[])
 
+    const filterGamesDate = (gameDate) => {
+        const gameDateObject = new Date(gameDate);
+        const gameMonth = gameDateObject.getMonth();
+        const gameDateDay = gameDateObject.getDate();
+        const gameYear = gameDateObject.getFullYear();
+        if(toggledDate.getDate() == gameDateDay && toggledDate.getMonth() == gameMonth && toggledDate.getFullYear() == gameYear){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    const handleDateChange = (currentToggledDate, directionFlag) => {
+        const currentDate = currentToggledDate;
+        const newDate = new Date(currentDate)
+        if(directionFlag){
+            newDate.setDate(newDate.getDate() + 1)
+        }
+        else{
+            newDate.setDate(newDate.getDate() - 1)
+        }
+        setToggledDate(newDate)
+    }
+    
 
     return (
         <Grid sx={customStyles.upcomingContainer}>
             <Grid item xs={12}>
-                Upcoming Games
+                <Grid container>
+                    <Grid item xs={2}>
+                        <button onClick={()=>handleDateChange(toggledDate, false)}>
+                            back
+                        </button>
+                    </Grid>
+                    <Grid item xs={8}>{toggledDate.toDateString()}</Grid>
+                    <Grid item xs={2}>
+                        <button onClick={()=>handleDateChange(toggledDate, true)}>
+                            next
+                        </button>
+                    </Grid>
+                </Grid>
             </Grid>
             <Grid item xs={12}>
                 {allGames ? allGames.map((eachGame)=>{
-                    return(
-                        <NBAgame gameObject={eachGame} />
-                    )
+                    if(filterGamesDate(eachGame.gameStartDate)){
+                        return(
+                            <NBAgame gameObject={eachGame} />
+                        )
+                    }
                 }) : null}
             </Grid>
         </Grid>
